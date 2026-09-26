8fa328ff36ba3c408cfed72f3a5b3b2cb89fdeaf
vishalshinde-data <vishalshinde.tech@gmail.com>
2026-09-26 12:13:45 +0530

    added

--- Core Python/Demos/Data Structure.py/List.py/Duplicate.py
+++ Core Python/Demos/Data Structure.py/List.py/Duplicate.py
@@ -5,3 +5,12 @@ for i in range(0, len(a)):
     for j in range(i+1, len(a)):
         if(a[i] == a[j]):
             print(a[i])
+
+
+
+a = [10, 30, 20, 80, 10, 60, 20]
+for i in range(0, len(a)):
+    for j in range(i+1, len(a)):
+        if(a[i] == a[j]):
+            print(a[i])
+
